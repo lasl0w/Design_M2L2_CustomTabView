5dd02af94bc8acc0368768aeb828dd53687c26e0
Tom Montgomery <montgomery3681@gmail.com>
2023-10-02 21:46:48 -0600

    Initial Commit

---
+++ Design_M2L2_CustomTabView/Design_M2L2_CustomTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_M2L2_CustomTabViewApp.swift
+//  Design_M2L2_CustomTabView
+//
+//  Created by tom montgomery on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Design_M2L2_CustomTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
